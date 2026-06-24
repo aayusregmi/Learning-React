e82753606277fa35f9ab0f1de1d927a147b078e0
Aayus Regmi <aayusregmi721@gmail.com>
2026-06-24 22:14:30 +0545

    Get knowledge about how react linked to html , why createRoot method and about component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Lemontea from "./App2"
 const App = () => {
   return (
     <div>
       <h1>Hello Aayus SO excited to learn React!!</h1>
+      <Lemontea />
     </div>
   )
 }
---
+++ src/App2.jsx
@@ -0,0 +1,5 @@
+const Lemontea = () => {
+  return <h1>Hello, I am learning react from chai aur code</h1>;
+};
+
+export default Lemontea
